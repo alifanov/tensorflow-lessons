56d0542af7de3be85f3a6a4e0fd7ea8543dd2029
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-03-17 14:57:20 +0300

    test prettytensor

--- hvass/03_prettytensor.py
+++ hvass/03_prettytensor.py
@@ -6,6 +6,9 @@ import time
 from datetime import timedelta
 import math
 
+# number of CPU
+NUM_THREADS = 8
+
 # Convolutional Layer 1.
 filter_size1 = 5  # Convolution filters are 5 x 5 pixels.
 num_filters1 = 32  # There are 16 of these filters.
@@ -18,6 +21,7 @@ num_filters2 = 64  # There are 36 of these filters.
 fc_size = 128  # Number of neurons in fully-connected layer.
 
 from tensorflow.examples.tutorials.mnist import input_data
+import prettytensor as pt
 
 data = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -42,116 +46,116 @@ num_channels = 1
 num_classes = 10
 
 
-def new_weights(shape):
-    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+# def new_weights(shape):
+#     return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+#
+#
+# def new_biases(length):
+#     return tf.Variable(tf.constant(0.05, shape=[length]))
 
 
-def new_biases(length):
-    return tf.Variable(tf.constant(0.05, shape=[length]))
+# def new_conv_layer(input,  # The previous layer.
+#                    num_input_channels,  # Num. channels in prev. layer.
+#                    filter_size,  # Width and height of each filter.
+#                    num_filters,  # Number of filters.
+#                    use_pooling=True):  # Use 2x2 max-pooling.
+#
+#     # Shape of the filter-weights for the convolution.
+#     # This format is determined by the TensorFlow API.
+#     shape = [filter_size, filter_size, num_input_channels, num_filters]
+#
+#     # Create new weights aka. filters with the given shape.
+#     weights = new_weights(shape=shape)
+#
+#     # Create new biases, one for each filter.
+#     biases = new_biases(length=num_filters)
+#
+#     # Create the TensorFlow operation for convolution.
+#     # Note the strides are set to 1 in all dimensions.
+#     # The first and last stride must always be 1,
+#     # because the first is for the image-number and
+#     # the last is for the input-channel.
+#     # But e.g. strides=[1, 2, 2, 1] would mean that the filter
+#     # is moved 2 pixels across the x- and y-axis of the image.
+#     # The padding is set to 'SAME' which means the input image
+#     # is padded with zeroes so the size of the output is the same.
+#     layer = tf.nn.conv2d(input=input,
+#                          filter=weights,
+#                          strides=[1, 1, 1, 1],
+#                          padding='SAME')
+#
+#     # Add the biases to the results of the convolution.
+#     # A bias-value is added to each filter-channel.
+#     layer += biases
+#
+#     # Use pooling to down-sample the image resolution?
+#     if use_pooling:
+#         # This is 2x2 max-pooling, which means that we
+#         # consider 2x2 windows and select the largest value
+#         # in each window. Then we move 2 pixels to the next window.
+#         layer = tf.nn.max_pool(value=layer,
+#                                ksize=[1, 2, 2, 1],
+#                                strides=[1, 2, 2, 1],
+#                                padding='SAME')
+#
+#     # Rectified Linear Unit (ReLU).
+#     # It calculates max(x, 0) for each input pixel x.
+#     # This adds some non-linearity to the formula and allows us
+#     # to learn more complicated functions.
+#     layer = tf.nn.relu(layer)
+#
+#     # Note that ReLU is normally executed before the pooling,
+#     # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+#     # save 75% of the relu-operations by max-pooling first.
+#
+#     # We return both the resulting layer and the filter-weights
+#     # because we will plot the weights later.
+#     return layer, weights
 
 
-def new_conv_layer(input,  # The previous layer.
-                   num_input_channels,  # Num. channels in prev. layer.
-                   filter_size,  # Width and height of each filter.
-                   num_filters,  # Number of filters.
-                   use_pooling=True):  # Use 2x2 max-pooling.
-
-    # Shape of the filter-weights for the convolution.
-    # This format is determined by the TensorFlow API.
-    shape = [filter_size, filter_size, num_input_channels, num_filters]
-
-    # Create new weights aka. filters with the given shape.
-    weights = new_weights(shape=shape)
-
-    # Create new biases, one for each filter.
-    biases = new_biases(length=num_filters)
-
-    # Create the TensorFlow operation for convolution.
-    # Note the strides are set to 1 in all dimensions.
-    # The first and last stride must always be 1,
-    # because the first is for the image-number and
-    # the last is for the input-channel.
-    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
-    # is moved 2 pixels across the x- and y-axis of the image.
-    # The padding is set to 'SAME' which means the input image
-    # is padded with zeroes so the size of the output is the same.
-    layer = tf.nn.conv2d(input=input,
-                         filter=weights,
-                         strides=[1, 1, 1, 1],
-                         padding='SAME')
-
-    # Add the biases to the results of the convolution.
-    # A bias-value is added to each filter-channel.
-    layer += biases
-
-    # Use pooling to down-sample the image resolution?
-    if use_pooling:
-        # This is 2x2 max-pooling, which means that we
-        # consider 2x2 windows and select the largest value
-        # in each window. Then we move 2 pixels to the next window.
-        layer = tf.nn.max_pool(value=layer,
-                               ksize=[1, 2, 2, 1],
-                               strides=[1, 2, 2, 1],
-                               padding='SAME')
-
-    # Rectified Linear Unit (ReLU).
-    # It calculates max(x, 0) for each input pixel x.
-    # This adds some non-linearity to the formula and allows us
-    # to learn more complicated functions.
-    layer = tf.nn.relu(layer)
-
-    # Note that ReLU is normally executed before the pooling,
-    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
-    # save 75% of the relu-operations by max-pooling first.
-
-    # We return both the resulting layer and the filter-weights
-    # because we will plot the weights later.
-    return layer, weights
+# def flatten_layer(layer):
+#     # Get the shape of the input layer.
+#     layer_shape = layer.get_shape()
+#
+#     # The shape of the input layer is assumed to be:
+#     # layer_shape == [num_images, img_height, img_width, num_channels]
+#
+#     # The number of features is: img_height * img_width * num_channels
+#     # We can use a function from TensorFlow to calculate this.
+#     num_features = layer_shape[1:4].num_elements()
+#
+#     # Reshape the layer to [num_images, num_features].
+#     # Note that we just set the size of the second dimension
+#     # to num_features and the size of the first dimension to -1
+#     # which means the size in that dimension is calculated
+#     # so the total size of the tensor is unchanged from the reshaping.
+#     layer_flat = tf.reshape(layer, [-1, num_features])
+#
+#     # The shape of the flattened layer is now:
+#     # [num_images, img_height * img_width * num_channels]
+#
+#     # Return both the flattened layer and the number of features.
+#     return layer_flat, num_features
 
 
-def flatten_layer(layer):
-    # Get the shape of the input layer.
-    layer_shape = layer.get_shape()
-
-    # The shape of the input layer is assumed to be:
-    # layer_shape == [num_images, img_height, img_width, num_channels]
-
-    # The number of features is: img_height * img_width * num_channels
-    # We can use a function from TensorFlow to calculate this.
-    num_features = layer_shape[1:4].num_elements()
-
-    # Reshape the layer to [num_images, num_features].
-    # Note that we just set the size of the second dimension
-    # to num_features and the size of the first dimension to -1
-    # which means the size in that dimension is calculated
-    # so the total size of the tensor is unchanged from the reshaping.
-    layer_flat = tf.reshape(layer, [-1, num_features])
-
-    # The shape of the flattened layer is now:
-    # [num_images, img_height * img_width * num_channels]
-
-    # Return both the flattened layer and the number of features.
-    return layer_flat, num_features
-
-
-def new_fc_layer(input,  # The previous layer.
-                 num_inputs,  # Num. inputs from prev. layer.
-                 num_outputs,  # Num. outputs.
-                 use_relu=True):  # Use Rectified Linear Unit (ReLU)?
-
-    # Create new weights and biases.
-    weights = new_weights(shape=[num_inputs, num_outputs])
-    biases = new_biases(length=num_outputs)
-
-    # Calculate the layer as the matrix multiplication of
-    # the input and weights, and then add the bias-values.
-    layer = tf.matmul(input, weights) + biases
-
-    # Use ReLU?
-    if use_relu:
-        layer = tf.nn.relu(layer)
-
-    return layer
+# def new_fc_layer(input,  # The previous layer.
+#                  num_inputs,  # Num. inputs from prev. layer.
+#                  num_outputs,  # Num. outputs.
+#                  use_relu=True):  # Use Rectified Linear Unit (ReLU)?
+#
+#     # Create new weights and biases.
+#     weights = new_weights(shape=[num_inputs, num_outputs])
+#     biases = new_biases(length=num_outputs)
+#
+#     # Calculate the layer as the matrix multiplication of
+#     # the input and weights, and then add the bias-values.
+#     layer = tf.matmul(input, weights) + biases
+#
+#     # Use ReLU?
+#     if use_relu:
+#         layer = tf.nn.relu(layer)
+#
+#     return layer
 
 
 x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='x')
@@ -159,33 +163,45 @@ x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
 y_true = tf.placeholder(tf.float32, shape=[None, 10], name='y_true')
 y_true_cls = tf.argmax(y_true, dimension=1)
 
-layer_conv1, weights_conv1 = \
-    new_conv_layer(input=x_image,
-                   num_input_channels=num_channels,
-                   filter_size=filter_size1,
-                   num_filters=num_filters1,
-                   use_pooling=True)
+x_pretty = pt.wrap(x_image)
 
-layer_conv2, weights_conv2 = \
-    new_conv_layer(input=layer_conv1,
-                   num_input_channels=num_filters1,
-                   filter_size=filter_size2,
-                   num_filters=num_filters2,
-                   use_pooling=True)
+with pt.defaults_scope(activation_fn=tf.nn.relu):
+    y_pred, loss = x_pretty.\
+        conv2d(kernel=5, depth=32, name='layer_conv1').\
+        max_pool(kernel=2, stride=2).\
+        conv2d(kernel=5, depth=64, name='layer_conv2').\
+        max_pool(kernel=2, stride=2).\
+        flatten().\
+        fully_connected(size=1024, name='layer_fc1').\
+        softmax_classifier(num_classes=num_classes, labels=y_true)
 
-layer_flat, num_features = flatten_layer(layer_conv2)
-
-layer_fc1 = new_fc_layer(input=layer_flat,
-                         num_inputs=num_features,
-                         num_outputs=fc_size,
-                         use_relu=True)
-
-layer_fc2 = new_fc_layer(input=layer_fc1,
-                         num_inputs=fc_size,
-                         num_outputs=num_classes,
-                         use_relu=False)
-
-y_pred = tf.nn.softmax(layer_fc2, name='y_pred')
+# layer_conv1, weights_conv1 = \
+#     new_conv_layer(input=x_image,
+#                    num_input_channels=num_channels,
+#                    filter_size=filter_size1,
+#                    num_filters=num_filters1,
+#                    use_pooling=True)
+#
+# layer_conv2, weights_conv2 = \
+#     new_conv_layer(input=layer_conv1,
+#                    num_input_channels=num_filters1,
+#                    filter_size=filter_size2,
+#                    num_filters=num_filters2,
+#                    use_pooling=True)
+#
+# layer_flat, num_features = flatten_layer(layer_conv2)
+#
+# layer_fc1 = new_fc_layer(input=layer_flat,
+#                          num_inputs=num_features,
+#                          num_outputs=fc_size,
+#                          use_relu=True)
+#
+# layer_fc2 = new_fc_layer(input=layer_fc1,
+#                          num_inputs=fc_size,
+#                          num_outputs=num_classes,
+#                          use_relu=False)
+#
+# y_pred = tf.nn.softmax(layer_fc2, name='y_pred')
 y_pred_cls = tf.argmax(y_pred, dimension=1)
 
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y_pred,
@@ -200,11 +216,11 @@ correct_prediction = tf.equal(y_pred_cls, y_true_cls)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 tf.summary.scalar('accuracy', accuracy)
 
-train_batch_size = 64
+train_batch_size = 50
 
 total_iterations = 0
 
-test_batch_size = 256
+test_batch_size = 100
 
 merged = tf.summary.merge_all()
 
@@ -317,7 +333,8 @@ def optimize(session, num_iterations, writer=None):
     print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
 
-with tf.Session() as session:
+with tf.Session(config=tf.ConfigProto(
+  intra_op_parallelism_threads=NUM_THREADS)) as session:
     writer = tf.summary.FileWriter('./tf_logs', graph=session.graph)
     session.run(tf.global_variables_initializer())
     optimize(session, 25001, writer=writer)
