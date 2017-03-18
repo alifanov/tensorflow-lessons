cf2e7b1c28bbf5e954cea2e20bcbf5b8dff8ecb5
alifanov <lifanov.a.v@gmail.com>
2017-03-18 19:18:29 +0300

    add cifar10

---
+++ hvass/06_cifar.py
@@ -0,0 +1,203 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import time
+from datetime import timedelta
+import math
+import os
+
+import prettytensor as pt
+
+import cifar10
+
+cifar10.maybe_download_and_extract()
+images_train, cls_train, labels_train = cifar10.load_training_data()
+images_test, cls_test, labels_test = cifar10.load_test_data()
+
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(images_train)))
+print("- Test-set:\t\t{}".format(len(images_test)))
+
+from cifar10 import img_size, num_channels, num_classes
+
+img_size_cropped = 24
+
+x = tf.placeholder(tf.float32, shape=[None, img_size, img_size, num_channels], name='x')
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+
+def pre_process_image(image, training):
+    if training:
+        image = tf.random_crop(image, size=[img_size_cropped, img_size_cropped, num_channels])
+
+        image = tf.image.random_flip_left_right(image)
+
+        image = tf.image.random_hue(image, max_delta=0.05)
+        image = tf.image.random_contrast(image, lower=0.3, upper=1.0)
+        image = tf.image.random_brightness(image, max_delta=0.2)
+        image = tf.image.random_saturation(image, lower=0.0, upper=2.0)
+
+        image = tf.minimum(image, 1.0)
+        image = tf.maximum(image, 0.0)
+    else:
+        image = tf.image.resize_image_with_crop_or_pad(image,
+                                                       target_height=img_size_cropped,
+                                                       target_width=img_size_cropped)
+
+    return image
+
+
+def pre_process(images, training):
+    return tf.map_fn(lambda image: pre_process_image(image, training), images)
+
+
+# distorted_images = pre_process(images=x, training=True)
+
+
+def main_network(images, training):
+    x_pretty = pt.wrap(images)
+
+    if training:
+        phase = pt.Phase.train
+    else:
+        phase = pt.Phase.infer
+
+    # Create the convolutional neural network using Pretty Tensor.
+    # It is very similar to the previous tutorials, except
+    # the use of so-called batch-normalization in the first layer.
+    with pt.defaults_scope(activation_fn=tf.nn.relu, phase=phase):
+        y_pred, loss = x_pretty. \
+            conv2d(kernel=5, depth=64, name='layer_conv1', batch_normalize=True). \
+            max_pool(kernel=2, stride=2). \
+            conv2d(kernel=5, depth=64, name='layer_conv2'). \
+            max_pool(kernel=2, stride=2). \
+            flatten(). \
+            fully_connected(size=256, name='layer_fc1'). \
+            fully_connected(size=128, name='layer_fc2'). \
+            softmax_classifier(num_classes=num_classes, labels=y_true)
+
+    return y_pred, loss
+
+
+def create_network(training):
+    # Wrap the neural network in the scope named 'network'.
+    # Create new variables during training, and re-use during testing.
+    with tf.variable_scope('network', reuse=not training):
+        # Just rename the input placeholder variable for convenience.
+        images = x
+
+        # Create TensorFlow graph for pre-processing.
+        images = pre_process(images=images, training=training)
+
+        # Create TensorFlow graph for the main processing.
+        y_pred, loss = main_network(images=images, training=training)
+
+    return y_pred, loss
+
+
+global_step = tf.Variable(initial_value=0,
+                          name='global_step', trainable=False)
+
+_, loss = create_network(training=True)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, global_step=global_step)
+y_pred, _ = create_network(training=False)
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+train_batch_size = 64
+
+
+def random_batch():
+    # Number of images in the training-set.
+    num_images = len(images_train)
+
+    # Create a random index.
+    idx = np.random.choice(num_images,
+                           size=train_batch_size,
+                           replace=False)
+
+    # Use the random index to select random images and labels.
+    x_batch = images_train[idx, :, :, :]
+    y_batch = labels_train[idx, :]
+
+    return x_batch, y_batch
+
+
+def optimize(num_iterations):
+    for i in range(num_iterations):
+        # Get a batch of training examples.
+        # x_batch now holds a batch of images and
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch = random_batch()
+
+        # Put the batch into a dict with the proper names
+        # for placeholder variables in the TensorFlow graph.
+        feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        # We also want to retrieve the global_step counter.
+        i_global, _ = session.run([global_step, optimizer],
+                                  feed_dict=feed_dict_train)
+
+        # Print status to screen every 100 iterations (and last).
+        if (i_global % 100 == 0) or (i == num_iterations - 1):
+            # Calculate the accuracy on the training-batch.
+            batch_acc = session.run(accuracy,
+                                    feed_dict=feed_dict_train)
+
+            print("Global Step: {0:>6}, Training Batch Accuracy: {1:>6.1%}".format(i_global, batch_acc))
+
+
+# Split the data-set in batches of this size to limit RAM usage.
+batch_size = 256
+
+
+def predict_cls(images, labels, cls_true):
+    # Number of images.
+    num_images = len(images)
+
+    # Allocate an array for the predicted classes which
+    # will be calculated in batches and filled into this array.
+    cls_pred = np.zeros(shape=num_images, dtype=np.int)
+
+    # Now calculate the predicted classes for the batches.
+    # We will just iterate through all the batches.
+    # There might be a more clever and Pythonic way of doing this.
+
+    # The starting index for the next batch is denoted i.
+    i = 0
+
+    while i < num_images:
+        # The ending index for the next batch is denoted j.
+        j = min(i + batch_size, num_images)
+
+        # Create a feed-dict with the images and labels
+        # between index i and j.
+        feed_dict = {x: images[i:j, :],
+                     y_true: labels[i:j, :]}
+
+        # Calculate the predicted class using TensorFlow.
+        cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
+
+        # Set the start-index for the next batch to the
+        # end-index of the current batch.
+        i = j
+
+    # Create a boolean array whether each image is correctly classified.
+    correct = (cls_true == cls_pred)
+
+    return correct, cls_pred
+
+
+with tf.Session() as session:
+    session.run(tf.global_variables_initializer())
+    optimize(1000)
+    correct, _ = predict_cls(images_test, labels_test, cls_test)
+    print('Accuracy {0:.4}%'.format(correct.mean()))
