5e03ecbc9187dca1bee8dac05ca26cb345e96e39
alifanov <lifanov.a.v@gmail.com>
2017-03-28 21:02:52 +0300

    add gridsearch

--- kaggle/houses_price/v2.py
+++ kaggle/houses_price/v2.py
@@ -4,74 +4,124 @@ import seaborn as sns
 
 from sklearn import preprocessing
 from sklearn import cross_validation
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
 
 from keras.layers import Dense, Dropout, Activation
 from keras.models import Sequential
 from keras.optimizers import SGD, Adam
 from matplotlib import pyplot
 
-data = pd.read_csv('./train.csv')
-print('Data shape: {}'.format(data.shape))
-
-data_test = pd.read_csv('./test.csv')
 TARGET_COLUMN = 'SalePrice'
 
-categorical_fields = data.select_dtypes(exclude=[np.number]).columns
-# print(categorical_fields)
+data_test = pd.read_csv('./test.csv')
 
-num_fields = data.select_dtypes(include=[np.number]).drop(TARGET_COLUMN, 1).columns
-# print(num_fields)
 
-for col in categorical_fields:
-    data[col].fillna('default', inplace=True)
-    data_test[col].fillna('default', inplace=True)
+def prepare_data():
+    data = pd.read_csv('./train.csv')
 
-for col in num_fields:
-    data[col].fillna(0, inplace=True)
-    data_test[col].fillna(0, inplace=True)
+    categorical_fields = data.select_dtypes(exclude=[np.number]).columns
+    # print(categorical_fields)
 
-encode = preprocessing.LabelEncoder()
-for col in categorical_fields:
-    data[col] = encode.fit_transform(data[col])
-    data_test[col] = encode.fit_transform(data_test[col])
+    num_fields = data.select_dtypes(include=[np.number]).drop(TARGET_COLUMN, 1).columns
+    # print(num_fields)
 
-data[TARGET_COLUMN].fillna(0, inplace=True)
+    for col in categorical_fields:
+        data[col].fillna('default', inplace=True)
+        data_test[col].fillna('default', inplace=True)
 
-y = data[TARGET_COLUMN].as_matrix()
-X = data.drop(TARGET_COLUMN, 1)
-X = X.drop('Id', 1).as_matrix()
+    for col in num_fields:
+        data[col].fillna(0, inplace=True)
+        data_test[col].fillna(0, inplace=True)
+
+    encode = preprocessing.LabelEncoder()
+    for col in categorical_fields:
+        data[col] = encode.fit_transform(data[col])
+        data_test[col] = encode.fit_transform(data_test[col])
+
+    data[TARGET_COLUMN].fillna(0, inplace=True)
+
+    X = data.drop(TARGET_COLUMN, 1)
+    X = X.drop('Id', 1).as_matrix()
+    y = data[TARGET_COLUMN].as_matrix()
+    X_test = data_test.drop('Id', 1).as_matrix()
+
+    return X, y, X_test
+
+
+def create_model(n_input, n_epochs=1000, optimizer='Adam'):
+    model = Sequential()
+
+    model.add(Dense(n_input, input_dim=n_input, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    # model.add(Dense(512, activation='relu'))
+    # model.add(Dense(256, activation='relu'))
+    # model.add(Dense(128, activation='relu'))
+    model.add(Dense(1))
+
+    # learning_rate = 1e-4
+    # decay = learning_rate / n_epochs
+
+    model.compile(loss='mse', optimizer=optimizer)
+    return model
+
+
+X, y, X_test = prepare_data()
 
 n_input = X.shape[1]
 
-x_scaler = preprocessing.MinMaxScaler()
-y_scaler = preprocessing.MinMaxScaler()
+# x_scaler = preprocessing.MinMaxScaler()
+# y_scaler = preprocessing.MinMaxScaler()
+#
+# X_scaled = x_scaler.fit_transform(X)
+# y_scaled = y_scaler.fit_transform(y.reshape(-1, 1))
 
-X_scaled = x_scaler.fit_transform(X)
-y_scaled = y_scaler.fit_transform(y.reshape(-1, 1))
-
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-    X_scaled, y_scaled, test_size=0.1, random_state=3)
+# X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+#     X_scaled, y_scaled, test_size=0.1, random_state=3)
 
 # print('X_train: {}'.format(X_train.shape))
 # print('X_test: {}'.format(X_test.shape))
 # print('y_train: {}'.format(y_train.shape))
 # print('y_test: {}'.format(y_test.shape))
 
-model = Sequential()
+# n_epochs = 100
+# model = create_model(n_epochs)
+# np.random.seed(3)
+# model.fit(X, y, epochs=n_epochs, batch_size=10, verbose=1)
 
-model.add(Dense(1024, input_dim=n_input, activation='relu'))
-# model.add(Dense(512, activation='relu'))
-model.add(Dense(256, activation='relu'))
-# model.add(Dense(128, activation='relu'))
-model.add(Dense(1))
+# GridSearchCV
+model = KerasRegressor(build_fn=create_model, n_input=n_input, n_epochs=100, batch_size=5, verbose=0)
+param_grid = {
+    'n_epochs': [50, 100, 200, 500],
+    'batch_size': [50, 75, 100],
+    'optimizer': ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+}
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+grid_result = grid.fit(X, y)
 
-learning_rate = 1e-4
-n_epochs = 1000
-decay = learning_rate / n_epochs
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+print(dir(grid_result))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
 
-model.compile(loss='mse', optimizer=Adam(lr=learning_rate), metrics=['accuracy'], decay=decay)
-np.random.seed(3)
-model.fit(X_train, y_train, epochs=n_epochs, batch_size=10, verbose=1)
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# seed = 7
+# np.random.seed(seed)
+# estimators = []
+# estimators.append(('standardize', StandardScaler()))
+# estimators.append(('mlp', KerasRegressor(build_fn=create_model, n_input=n_input, n_epochs=100, batch_size=5, verbose=0)))
+# pipeline = Pipeline(estimators)
+#
+# kfold = KFold(n_splits=3, random_state=seed)
+# results = cross_val_score(pipeline, X, y, cv=kfold)
+# print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
 
 # predicted = model.predict(X_test)
 
@@ -81,24 +131,15 @@ model.fit(X_train, y_train, epochs=n_epochs, batch_size=10, verbose=1)
 
 
 
-X_test = data_test.drop('Id', 1)
-X_test_scaled = x_scaler.fit_transform(X_test)
-print(X_test_scaled.shape)
+# X_test_scaled = x_scaler.fit_transform(X_test)
+# print(X_test_scaled.shape)
+#
+# y_pred = model.predict(X_test_scaled)
+# y_pred = y_scaler.inverse_transform(y_pred)
+# print(y_pred.shape)
 
-y_pred = model.predict(X_test_scaled)
-y_pred = y_scaler.inverse_transform(y_pred)
-print(y_pred.shape)
-
-i = 0
-file = open('submission.csv', 'w')
-header = "Id,SalePrice"
-header = header + '\n'
-file.write(header)
-for id in (data_test['Id']):
-    str = "{},{}".format(id, y_pred[i][0])
-    str = str + '\n'
-    file.write(str)
-    i += 1
-
-# for id,y in zip(data_test['id'], y_pred):
+# file = open('submission.csv', 'w')
+# header = "Id,SalePrice\n"
+# file.write(header)
+# for id, y in zip(data_test['Id'], y_pred):
 #     file.write('{},{}\n'.format(id, y[0]))
