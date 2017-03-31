f2693ea7f6d63faac8037722341f851d802ad7c1
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-03-31 18:59:40 +0300

    edit

--- kaggle/houses_price/v2.py
+++ kaggle/houses_price/v2.py
@@ -2,19 +2,18 @@ import csv
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 from keras.wrappers.scikit_learn import KerasRegressor
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
 
 TARGET_COLUMN = 'SalePrice'
-EPOCHS = 300
-LR = 1e-2
-BATCH_SIZE = 50
+EPOCHS = 400
+LR = 5e-5
+BATCH_SIZE = 20
 
 data_test = pd.read_csv('./test.csv')
 
@@ -50,42 +49,54 @@ def prepare_data():
 
 def create_model(
         n_input,
-        activation='relu'
+        activation='relu',
+        dropout=0.5
 ):
     model = Sequential()
 
-    model.add(Dense(n_input, input_dim=n_input, activation=activation))
-    model.add(Dense(128, activation=activation))
-    model.add(Dense(64, activation=activation))
-    model.add(Dense(1))
+    model.add(Dense(1024, input_dim=n_input, activation=activation, kernel_initializer='uniform'))
+    model.add(Dropout(dropout))
+    # model.add(Dense(512, activation=activation))
+    # model.add(Dropout(dropout))
+    # model.add(Dense(256, activation=activation))
+    # model.add(Dropout(dropout))
+    # model.add(Dense(128, activation=activation))
+    # model.add(Dropout(dropout))
+    # model.add(Dense(64, activation=activation))
+    # model.add(Dropout(dropout))
+    # model.add(Dense(32, activation=activation))
+    # model.add(Dropout(dropout))
+    # model.add(Dense(16, activation=activation))
+    # model.add(Dropout(dropout))
+    model.add(Dense(1, activation=activation, kernel_initializer='uniform'))
 
     learning_rate = LR
     decay = learning_rate / EPOCHS
 
-    model.compile(loss='mse', optimizer=Adam(lr=learning_rate, decay=decay))
+    model.compile(loss='mse', optimizer=Adam(lr=LR, decay=decay))
     return model
 
 
 X, y, X_validation = prepare_data()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-# X_train = X
-# y_train = y
 
 n_input = X.shape[1]
-
-# x_scaler = preprocessing.MinMaxScaler()
-# X_scaled = x_scaler.fit_transform(X)
-# X_test = x_scaler.fit_transform(X_test)
-# X_train = X_scaled
-
 
 nb_epoch = EPOCHS
 np.random.seed(3)
 model = KerasRegressor(build_fn=create_model, n_input=n_input, epochs=nb_epoch, batch_size=BATCH_SIZE, verbose=1)
-model.fit(X_train, y_train)
+history = model.fit(X, y, validation_split=0.33)
 
-y_test_pred = model.predict(X_test)
-rmse_test = mean_squared_error(y_test, y_test_pred)**0.5
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# y_test_pred = model.predict(X_validation)
+# rmse_test = mean_squared_error(y_test, y_test_pred)**0.5
+rmse_test = np.mean(history.history['val_loss'])
 print()
 print('Test RMSE: {0:.4}'.format(rmse_test))
 
