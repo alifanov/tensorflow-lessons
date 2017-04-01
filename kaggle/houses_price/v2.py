52db9b36bb2f311ed19f68ccfa2d778338a7a968
alifanov <lifanov.a.v@gmail.com>
2017-04-01 21:17:11 +0300

    new attempt: bs_100-lr_0.001-e_500-rmse_4911477941.09

--- kaggle/houses_price/v2.py
+++ kaggle/houses_price/v2.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from git import Repo
 
 from sklearn import preprocessing
 from keras.wrappers.scikit_learn import KerasRegressor
@@ -11,9 +12,9 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 
 TARGET_COLUMN = 'SalePrice'
-EPOCHS = 400
-LR = 5e-5
-BATCH_SIZE = 20
+EPOCHS = 500
+LR = 1e-3
+BATCH_SIZE = 100
 
 data_test = pd.read_csv('./test.csv')
 
@@ -26,8 +27,8 @@ def prepare_data():
     num_fields = data.select_dtypes(include=[np.number]).drop(TARGET_COLUMN, 1).columns
 
     for col in categorical_fields:
-        data[col].fillna('default', inplace=True)
-        data_test[col].fillna('default', inplace=True)
+        data[col].fillna(data[col].mode()[0], inplace=True)
+        data_test[col].fillna(data_test[col].mode()[0], inplace=True)
 
     for col in num_fields:
         data[col].fillna(0, inplace=True)
@@ -40,9 +41,19 @@ def prepare_data():
 
     data[TARGET_COLUMN].fillna(data[TARGET_COLUMN].mean(), inplace=True)
 
-    X = data.values[:, 1:80]
-    y = data.values[:, 80]
+    data.drop(['Utilities', 'RoofMatl', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'Heating', 'LowQualFinSF',
+               'BsmtFullBath', 'BsmtHalfBath', 'Functional', 'GarageYrBlt', 'GarageArea', 'GarageCond', 'WoodDeckSF',
+               'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'PoolQC', 'Fence', 'MiscFeature', 'MiscVal'],
+              axis=1, inplace=True)
+    X = data.values[:, 1:-1]
+    y = data.values[:, -1]
+
+    data_test.drop(['Utilities', 'RoofMatl', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'Heating', 'LowQualFinSF',
+               'BsmtFullBath', 'BsmtHalfBath', 'Functional', 'GarageYrBlt', 'GarageArea', 'GarageCond', 'WoodDeckSF',
+               'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'PoolQC', 'Fence', 'MiscFeature', 'MiscVal'],
+              axis=1, inplace=True)
     X_test = data_test.values[:, 1:]
+
 
     return X, y, X_test
 
@@ -50,11 +61,11 @@ def prepare_data():
 def create_model(
         n_input,
         activation='relu',
-        dropout=0.5
+        dropout=0.2
 ):
     model = Sequential()
 
-    model.add(Dense(1024, input_dim=n_input, activation=activation, kernel_initializer='uniform'))
+    model.add(Dense(n_input, input_dim=n_input, activation=activation, kernel_initializer='uniform'))
     model.add(Dropout(dropout))
     # model.add(Dense(512, activation=activation))
     # model.add(Dropout(dropout))
@@ -86,16 +97,14 @@ np.random.seed(3)
 model = KerasRegressor(build_fn=create_model, n_input=n_input, epochs=nb_epoch, batch_size=BATCH_SIZE, verbose=1)
 history = model.fit(X, y, validation_split=0.33)
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
 
-# y_test_pred = model.predict(X_validation)
-# rmse_test = mean_squared_error(y_test, y_test_pred)**0.5
 rmse_test = np.mean(history.history['val_loss'])
 print()
 print('Test RMSE: {0:.4}'.format(rmse_test))
@@ -107,8 +116,15 @@ config = 'bs_{}-lr_{}-e_{}-rmse_{:.2f}'.format(
     rmse_test
 )
 
+repo = Repo('../..')
+git = repo.git
+git.commit('.', m='new attempt: {}'.format(config))
+config += '.{}'.format(repo.commit().hexsha)
+
 y_pred = model.predict(X_validation)
 writer = csv.writer(open('submission.{}.csv'.format(config), 'w'))
 writer.writerow(['Id', 'SalePrice'])
 for id, y in zip(data_test['Id'], y_pred):
     writer.writerow([id, y])
+
+git.push('origin')
