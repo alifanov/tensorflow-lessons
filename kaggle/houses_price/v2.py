2e7529110b0966dbbf333a3e4f83a57b3e33b2d1
alifanov <lifanov.a.v@gmail.com>
2017-04-01 21:23:53 +0300

    new attempt: bs_100-lr_0.001-e_500-rmse_3055812555.17

--- kaggle/houses_price/v2.py
+++ kaggle/houses_price/v2.py
@@ -57,10 +57,10 @@ def create_model(
     model = Sequential()
 
     model.add(Dense(n_input, input_dim=n_input, activation=activation, kernel_initializer='uniform'))
-    model.add(Dropout(dropout))
+    # model.add(Dropout(dropout))
     # model.add(Dense(512, activation=activation))
     # model.add(Dropout(dropout))
-    # model.add(Dense(256, activation=activation))
+    model.add(Dense(256, activation=activation))
     # model.add(Dropout(dropout))
     # model.add(Dense(128, activation=activation))
     # model.add(Dropout(dropout))
