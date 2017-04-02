a1dd1f334340e8c9e530de74e6e0a1b53d7f92a3
alifanov <lifanov.a.v@gmail.com>
2017-04-02 12:07:03 +0300

    new attempt: bs_100-lr_0.001-e_700-rmse_1418336051.73

--- kaggle/houses_price/v2.py
+++ kaggle/houses_price/v2.py
@@ -87,15 +87,15 @@ n_input = X.shape[1]
 nb_epoch = EPOCHS
 np.random.seed(3)
 model = KerasRegressor(build_fn=create_model, n_input=n_input, epochs=nb_epoch, batch_size=BATCH_SIZE, verbose=1)
-history = model.fit(X, y, validation_split=0.33)
+history = model.fit(X, y, validation_split=0.2)
 
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 rmse_test = history.history['val_loss'][-1]
 print()
