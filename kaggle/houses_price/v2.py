389d0ab28fd4783a66aea4b7afe0b54241c8db2c
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-04-05 21:20:37 +0300

    new attempt: bs_100-lr_0.001-e_700-rmse_1341343741.61

--- kaggle/houses_price/v2.py
+++ kaggle/houses_price/v2.py
@@ -11,29 +11,28 @@ from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
 
-np.random.seed(3)
-
 TARGET_COLUMN = 'SalePrice'
-EPOCHS = 1000
-LR = 1e-5
-BATCH_SIZE = 50
+EPOCHS = 700
+LR = 1e-3
+BATCH_SIZE = 100
+
+data_test = pd.read_csv('./test.csv')
 
 
 def prepare_data():
     data = pd.read_csv('./train.csv')
-    data_test = pd.read_csv('./test.csv')
 
     categorical_fields = data.select_dtypes(exclude=[np.number]).columns
 
     num_fields = data.select_dtypes(include=[np.number]).drop(TARGET_COLUMN, 1).columns
 
-    for col in num_fields:
-        data[col].fillna(data[col].mean(), inplace=True)
-        data_test[col].fillna(data[col].mean(), inplace=True)
-
     for col in categorical_fields:
-        data[col].fillna(data[col].mode()[0], inplace=True)
-        data_test[col].fillna(data[col].mode()[0], inplace=True)
+        data[col].fillna('default', inplace=True)
+        data_test[col].fillna('default', inplace=True)
+
+    for col in num_fields:
+        data[col].fillna(0, inplace=True)
+        data_test[col].fillna(0, inplace=True)
 
     encode = preprocessing.LabelEncoder()
     for col in categorical_fields:
@@ -41,23 +40,12 @@ def prepare_data():
         data_test[col] = encode.fit_transform(data_test[col])
 
     # data[TARGET_COLUMN].fillna(data[TARGET_COLUMN].mean(), inplace=True)
-
     data.dropna(subset=[TARGET_COLUMN], inplace=True)
 
-    corr = data.corr()
-    corr.sort_values([TARGET_COLUMN], ascending=False, inplace=True)
-    columns = ['Id', 'OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea',
-               'TotalBsmtSF', '1stFlrSF', 'FullBath', 'TotRmsAbvGrd', 'YearBuilt',
-               'YearRemodAdd', 'MasVnrArea', 'GarageYrBlt', 'Fireplaces', 'BsmtFinSF1',
-               'Foundation', 'LotFrontage', 'WoodDeckSF', '2ndFlrSF', 'OpenPorchSF',
-               'GarageType', 'HeatingQC', 'GarageFinish', 'KitchenQual', 'BsmtQual',
-               'ExterQual', 'SalePrice'][1:]
-    data = data[columns]
-
-    X = data.values[:, :-1]
+    X = data.values[:, 1:-1]
     y = data.values[:, -1]
 
-    X_test = data_test[columns[:-1]].values
+    X_test = data_test.values[:, 1:]
 
     return X, y, X_test
 
@@ -73,11 +61,11 @@ def create_model(
     # model.add(Dropout(dropout))
     model.add(Dense(512, activation=activation, kernel_initializer='uniform'))
     # model.add(Dropout(dropout))
-    # model.add(Dense(256, activation=activation, kernel_initializer='uniform'))
+    model.add(Dense(256, activation=activation, kernel_initializer='uniform'))
     # model.add(Dropout(dropout))
-    # model.add(Dense(128, activation=activation, kernel_initializer='uniform'))
+    # model.add(Dense(128, activation=activation))
     # model.add(Dropout(dropout))
-    # model.add(Dense(64, activation=activation, kernel_initializer='uniform'))
+    # model.add(Dense(64, activation=activation))
     # model.add(Dropout(dropout))
     # model.add(Dense(32, activation=activation))
     # model.add(Dropout(dropout))
@@ -93,19 +81,21 @@ def create_model(
 
 
 X, y, X_validation = prepare_data()
+
 n_input = X.shape[1]
 
 nb_epoch = EPOCHS
+np.random.seed(3)
 model = KerasRegressor(build_fn=create_model, n_input=n_input, epochs=nb_epoch, batch_size=BATCH_SIZE, verbose=1)
-history = model.fit(X, y, validation_split=0.2)
+history = model.fit(X, y, validation_split=0.33)
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
 
 rmse_test = history.history['val_loss'][-1]
 print()
@@ -126,9 +116,6 @@ config += '.hash-{}'.format(repo.commit().hexsha)
 y_pred = model.predict(X_validation)
 writer = csv.writer(open('submission.{}.csv'.format(config), 'w'))
 writer.writerow(['Id', 'SalePrice'])
-
-data_test = pd.read_csv('./test.csv')
-
 for id, y in zip(data_test['Id'], y_pred):
     writer.writerow([id, y])
 
